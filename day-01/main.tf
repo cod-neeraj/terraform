12228b9ee94dc3cd6b8eb98678114532feec83ba
cod-neeraj <coderdeveloper00@gmail.com>
2025-11-12 19:16:45 +0530

    gg

--- day-01/main.tf
+++ day-01/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_instance" "example" {
-    ami = "ami-0c55b159cbfafe1f0"
+    ami = "ami-0ecb62995f68bb549"
     instance_type = "t2.micro"
   
 }
